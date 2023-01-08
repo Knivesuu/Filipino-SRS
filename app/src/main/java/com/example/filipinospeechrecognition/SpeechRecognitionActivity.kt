1d8942523d51cca89fcd009de9e7bc931b15b4d6
Kiven <kelao@up.edu.ph>
2023-01-08 18:29:53 +0800

    gradle build and added activity

---
+++ app/src/main/java/com/example/filipinospeechrecognition/SpeechRecognitionActivity.kt
@@ -0,0 +1,11 @@
+package com.example.filipinospeechrecognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeechRecognitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speech_recognition)
+    }
+}
