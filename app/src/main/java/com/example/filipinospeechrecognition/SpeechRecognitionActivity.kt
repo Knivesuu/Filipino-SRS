10d48e8bb52e7d04c7387ec90e992d75376c1572
Kiven <kelao@up.edu.ph>
2023-01-08 22:31:09 +0800

    added xml and kt speech recog function

--- app/src/main/java/com/example/filipinospeechrecognition/SpeechRecognitionActivity.kt
+++ app/src/main/java/com/example/filipinospeechrecognition/SpeechRecognitionActivity.kt
@@ -2,10 +2,70 @@ package com.example.filipinospeechrecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import android.content.Intent
+import java.util.*
 
 class SpeechRecognitionActivity : AppCompatActivity() {
+
+    // on below line we are creating variables
+    // for text view and image view
+    lateinit var outputTV: TextView
+    lateinit var micIV: ImageView
+    // on below line we are creating a constant value
+    private val REQUEST_CODE_SPEECH_INPUT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_speech_recognition)
+
+        // initializing variables of list view with their ids.
+        outputTV = findViewById(R.id.idTVOutput)
+        micIV = findViewById(R.id.idIVMic)
+
+        // on below line we are adding on click
+        // listener for mic image view.
+        micIV.setOnClickListener {
+            // on below line we are calling speech recognizer intent.
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            // on below line we are passing language model
+            // and model free form in our intent
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+
+            // on below line we are passing our
+            // language as a default language.
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault()
+            )
+
+            // on below line we are specifying a prompt
+            // message as speak to text on below line.
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+
+            // on below line we are specifying a try catch block.
+            // in this block we are calling a start activity
+            // for result method and passing our result code.
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } catch (e: Exception) {
+                // on below line we are displaying error message in toast
+                Toast
+                    .makeText(
+                        this@SpeechRecognitionActivity, " " + e.message,
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+        }
+
+
     }
 }
